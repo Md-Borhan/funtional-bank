b86197a1ee806c4a399d776d0a9d8bc2baae4d21
Borhan Uddin <borhanm373@gmail.com>
2023-02-16 09:13:46 +0600

    responsive ok

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ const loginBtn = document.getElementById('login-btn').addEventListener('click', 
     const password = userPassword.value
 
     if (email === 'bank@gmail.com' && password === 'bank') {
-        window.location.href = 'http://127.0.0.1:5500/index2.html'
+        window.location.href = 'index2.html'
     } else {
         alert('invalid info')
     }
